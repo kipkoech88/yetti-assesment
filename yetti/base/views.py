0c9817cfd70840a5e22c4e657cd1c8ab1d4ff01f
kipkoech88 <hoseakipkoech88@gmail.com>
2023-09-10 20:14:50 +0300

    restricted access to authenticated users

--- yetti/base/views.py
+++ yetti/base/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import RegisterForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url = '/login')
 def home(request):
     return render(request, 'base/home.html')
 
